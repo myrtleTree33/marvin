be98ba5d58d5a0d0dae4b021498d42cc630806b0
jhtong <me@joeltong.org>
2018-10-01 22:25:51 +0800

    added Cache interface

---
+++ src/lib/caches/Cache.js
@@ -0,0 +1,21 @@
+class Cache {
+  async add(item) {
+    // TOOD implement
+  }
+
+  async explored(item) {
+    // TOOD implement
+  }
+
+  async next(item) {
+    // TOOD implement
+  }
+
+  async size() {
+    // TOOD implement
+  }
+
+  async empty() {
+    // TOOD implement
+  }
+}
